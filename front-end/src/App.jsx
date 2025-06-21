d8bb95558e3f14506e5562e4655cda449e9020e0
amjadsh92 <amjadsharafeddien@gmail.com>
2025-06-21 10:50:31 +0300

    front-end:feat:set a background image

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primeflex/primeflex.css';
+import './App.css'
+
+function App() {
+  
+  
+
+  return (
+    <div className="bg-hero w-full h-full">
+        
+      
+    </div>
+  )
+}
+
+export default App
